58d7d04a0c374adf8d0c4b439dbda8f3531222f1
Naoto Kubota <kubogen3@gmail.com>
2024-07-13 11:07:25 +0900

    出産予定日までの残り日数と現在の妊娠週数の計算を修正

--- src/app/page.js
+++ src/app/page.js
@@ -7,10 +7,15 @@ export default function Home() {
   const [weeks, setWeeks] = useState('');
   const [days, setDays] = useState('');
   const [daysLeft, setDaysLeft] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
 
   const calculatePregnancyWeeks = (dueDate) => {
     const dueDateObj = new Date(dueDate); // 出産予定日 → Dateオブジェクト
     const currentDate = new Date(); // 現在の日付
+
+    dueDateObj.setHours(0, 0, 0, 0); // 時間部分をリセットして0時にセット
+    currentDate.setHours(0, 0, 0, 0); // 時間部分をリセットして0時にセット
+
     const diffInMillis = dueDateObj - currentDate; // 出産予定日 - 現在の日付（ミリ秒）
     const diffInDays = Math.floor(diffInMillis / (1000 * 60 * 60 * 24)); // ミリ秒 → 日
 
@@ -21,8 +26,6 @@ export default function Home() {
     const weeks = Math.floor(currentPregnancyDays / 7); // 何週
     const days = Math.floor(currentPregnancyDays % 7); // 何日
 
-    console.log(diffInDays);
-
     return { weeks, days, diffInDays };
   };
 
@@ -31,17 +34,24 @@ export default function Home() {
     setWeeks(result.weeks);
     setDays(result.days);
     setDaysLeft(result.diffInDays);
+    setIsLoading(false);
   }, [dueDate]);
 
   return (
     <div>
       <div className="text-center py-10 px-5">
-        <p className="mb-2">出産予定日</p>
-        <p className="font-medium text-2xl">{dueDate}</p>
-        <p className="mt-2">あと{daysLeft}日</p>
+        <p>出産予定日</p>
+        <p className="font-medium text-2xl">2025.03.02</p>
       </div>
       <div className="text-center">
-        <p>現在{weeks}週{days}日</p>
+        {isLoading ? (
+          <span className="loader"></span>
+        ) : (
+          <div>
+            <p>現在{weeks}週{days}日</p>
+            <p className="mt-2">あと{daysLeft}日</p>
+          </div>
+        )}
       </div>
     </div>
   );
