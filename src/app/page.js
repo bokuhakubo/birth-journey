ea3b797cbd824237def308ea2b002fa2dc164212
Naoto Kubota <kubogen3@gmail.com>
2024-07-21 23:21:51 +0900

    デザイン調整

--- src/app/page.js
+++ src/app/page.js
@@ -39,43 +39,42 @@ export default function Home() {
 
   return (
     <div>
-      <div className="px-5 py-20">
+      <div className="px-5 pt-10 pb-20">
         {isLoading ? (
           <div className="text-center">
             <span className="loader"></span>
           </div>
         ) : (
           <>
-            <div className="flex justify-center">
-              <div className="stats stats-vertical lg:stats-horizontal shadow">
-                <div className="stat">
-                  <div className="stat-title">予定日</div>
-                  <div className="stat-value text-2xl">2025年3月2日</div>
-                </div>
-                <div className="stat">
-                  <div className="stat-title">現在</div>
-                  <div className="stat-value text-2xl">{weeks}週{days}日</div>
-                </div>
-                <div className="stat">
-                  <div className="stat-title">あと</div>
-                  <div className="stat-value text-2xl">{daysLeft}日</div>
-                </div>
+            <div className="max-w-sm">
+              <div className="bg-gray-100 rounded-xl p-4">
+                <p className="text-gray-600">予定日</p>
+                <p className="text-xl font-bold text-black">2025年3月2日</p>
+              </div>
+              <div className="bg-gray-100 rounded-xl p-4 mt-4">
+                <p className="text-gray-600">現在</p>
+                <p className="text-xl font-bold text-black">{weeks}週{days}日</p>
+              </div>
+              <div className="bg-gray-100 rounded-xl p-4 mt-4">
+                <p className="text-gray-600">あと</p>
+                <p className="text-xl font-bold text-black">{daysLeft}日</p>
               </div>
             </div>
-            <div className="mt-10 border-l-4 border-black pl-4">
+
+            <div className="mt-20 max-w-sm">
               <div>
-                <p>7/6（土）</p>
-                <p>胎嚢(たいのう)確認</p>
+                <p className="font-mono font-medium text-sm text-gray-600 border-b border-gray-600 mb-2 pb-1">7/6（土）</p>
+                <p className="font-medium">胎嚢(たいのう)確認</p>
                 <p></p>
               </div>
               <div className="mt-8">
-                <p>7/20（土）</p>
-                <p>心拍確認</p>
-                <p>1.2センチ</p>
+                <p className="font-mono font-medium text-sm text-gray-600 border-b border-gray-600 mb-2 pb-1">7/20（土）</p>
+                <p className="font-medium">心拍確認</p>
+                <p className="text-sm mt-2">1.2センチに成長</p>
               </div>
               <div className="mt-8">
-                <p>7/29（月）</p>
-                <p>診察予定</p>
+                <p className="font-mono font-medium text-sm text-gray-600 border-b border-gray-600 mb-2 pb-1">7/29（月）</p>
+                <p className="font-medium">診察予定</p>
                 <p></p>
               </div>
             </div>
