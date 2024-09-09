8efa890ad42b62e49a2cf3332013291a0541f591
Naoto Kubota <kubogen3@gmail.com>
2024-09-10 08:24:03 +0900

    赤ちゃん画像追加

--- src/app/page.js
+++ src/app/page.js
@@ -92,18 +92,23 @@ export default function Home() {
           </div>
         ) : (
           <>
-            <div className="max-w-sm mx-auto">
-              <div className="bg-white rounded-xl p-4">
+            <div className="max-w-sm mx-auto bg-white rounded-xl p-4">
+              <div className="grid grid-cols-2">
+                <div>
+                  <p className="text-gray-600">あと</p>
+                  <p className="text-xl font-bold text-black">{daysLeft}日</p>
+                </div>
+                <div>
+                  <p className="text-gray-600">現在</p>
+                  <p className="text-xl font-bold text-black">{weeks}週{days}日（{months}ヶ月）</p>
+                </div>
+              </div>
+              <div>
+                <img src="/baby.png" />
+              </div>
+              <div>
                 <p className="text-gray-600">予定日</p>
                 <p className="text-xl font-bold text-black">2025年3月2日</p>
-              </div>
-              <div className="bg-white rounded-xl p-4 mt-4">
-                <p className="text-gray-600">現在</p>
-                <p className="text-xl font-bold text-black">{weeks}週{days}日（{months}ヶ月）</p>
-              </div>
-              <div className="bg-white rounded-xl p-4 mt-4">
-                <p className="text-gray-600">あと</p>
-                <p className="text-xl font-bold text-black">{daysLeft}日</p>
               </div>
             </div>
             <div className="mt-10 max-w-sm mx-auto">
