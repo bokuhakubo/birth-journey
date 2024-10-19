438d70e617b0b4c1c0be472d266a2722cbb42558
Naoto Kubota <kubogen3@gmail.com>
2024-10-19 17:45:08 +0900

    fix: pwa flow

--- src/app/components/InfoSection.jsx
+++ src/app/components/InfoSection.jsx
@@ -33,32 +33,32 @@ export default function InfoSection({ daysLeft, weeks, days, months, isPWA }) {
         {!isPWA && (
           <div className='max-w-sm mx-auto mt-16'>
             <h2 className='font-bold text-xl mb-4'>ホーム画面に追加する手順</h2>
-            <p>下記手順に従ってホーム画面に追加すればアプリとして使っていただけます。</p>
-            <div className='mt-8 p-4 bg-white rounded-xl'>
+            <p className='text-sm'>下記手順に従ってホーム画面に追加すればアプリのように使えます。</p>
+            <div className='mt-8 p-4 bg-white border border-gray-200 rounded-xl'>
               <h3 className='font-medium mb-2'>iPhone / iPadの場合（Safari）</h3>
               <ol className='list-decimal pl-4 text-sm'>
                 <li className='mb-2'>画面下の 「共有」 アイコン（四角と上向き矢印）をタップします。</li>
                 <li className='mb-2'>スクロールして 「ホーム画面に追加」 を選択します。</li>
                 <li className='mb-2'>任意の名前を入力し、「追加」をタップします。</li>
-                <li className='mb-2'>完了！ ホーム画面にアイコンが表示されます。</li>
+                <li className='mb-2'>ホーム画面にアイコンが表示されます。</li>
               </ol>
             </div>
-            <div className='mt-4 p-4 bg-white rounded-xl'>
+            <div className='mt-4 p-4 bg-white border border-gray-200 rounded-xl'>
               <h3 className='font-medium mb-2'>iPhone / iPadの場合（Chrome）</h3>
               <ol className='list-decimal pl-4 text-sm'>
                 <li className='mb-2'>画面右上の 共有 アイコン（四角と上向き矢印）をタップします。</li>
                 <li className='mb-2'>「ホーム画面に追加」 を選択します。</li>
                 <li className='mb-2'>名前を入力し、「追加」をタップします。</li>
-                <li className='mb-2'>完了！ ホーム画面にアイコンが表示されます。</li>
+                <li className='mb-2'>ホーム画面にアイコンが表示されます。</li>
               </ol>
             </div>
-            <div className='mt-4 p-4 bg-white rounded-xl'>
+            <div className='mt-4 p-4 bg-white border border-gray-200 rounded-xl'>
               <h3 className='font-medium mb-2'>Androidの場合</h3>
               <ol className='list-decimal pl-4 text-sm'>
                 <li className='mb-2'>画面右上の「︙」（メニューアイコン）をタップします。</li>
                 <li className='mb-2'>「ホーム画面に追加」 を選択します。</li>
                 <li className='mb-2'>表示されるポップアップで「追加」をタップします。</li>
-                <li className='mb-2'>完了！ ホーム画面にアイコンが表示されます。</li>
+                <li className='mb-2'>ホーム画面にアイコンが表示されます。</li>
               </ol>
             </div>
           </div>
