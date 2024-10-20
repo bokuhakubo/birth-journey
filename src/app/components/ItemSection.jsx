7c42ae367d809083431074ed5e81bba3e4ed0c9f
Naoto Kubota <kubogen3@gmail.com>
2024-10-20 09:22:45 +0900

    fix: update item

--- src/app/components/ItemSection.jsx
+++ src/app/components/ItemSection.jsx
@@ -348,6 +348,13 @@ export default function ItemSection() {
                                         <path d="M3.5 6.75c0-.69.56-1.25 1.25-1.25H7A.75.75 0 0 0 7 4H4.75A2.75 2.75 0 0 0 2 6.75v4.5A2.75 2.75 0 0 0 4.75 14h4.5A2.75 2.75 0 0 0 12 11.25V9a.75.75 0 0 0-1.5 0v2.25c0 .69-.56 1.25-1.25 1.25h-4.5c-.69 0-1.25-.56-1.25-1.25v-4.5Z" />
                                     </svg>
                                 </a>
+                                <a href="https://www.amazon.co.jp/%E3%83%8F%E3%83%B3%E3%82%AB%E3%83%81-4%E9%87%8D%E3%82%AC%E3%83%BC%E3%82%BC%E3%81%AE%E3%82%BF%E3%82%AA%E3%83%AB-%E7%B6%BF100-%E8%B5%A4%E3%81%A1%E3%82%83%E3%82%93%E3%82%AC%E3%83%BC%E3%82%BC-30x30%E3%8E%9D/dp/B07WYKCBRV/ref=mp_s_a_1_13?crid=IDR51UO4XJZ1&dib=eyJ2IjoiMSJ9.XgXU9p-683TIA7AJjr7soVhxfn6Vx5xqSNw5wIbVBorakpe-UDDdiHal3vBtvc2NotFWZd-0OlKUWZJnB6uh-7EbyrL2cOjkWs3fzh5JYIBYze1bE9MPNq_2EtIIWMQRT8xa6u9FxL2xMMdQgJZLEldRZ4Gl_p1RqPEIDtaXyCzC-1QW43ZSvBKghVxySogrgsuG4XNsW5kt5V0Pj3DNzQ.soBWs4pxspW6WnbKlmFw4hIv8q4Av9QiFmrTfkc9PrU&dib_tag=se&keywords=%E3%83%99%E3%83%93%E3%83%BC%2B%E3%82%AC%E3%83%BC%E3%82%BC%2B%E8%9B%8D%E5%85%89%E5%A1%97%E6%96%99%E4%B8%8D%E4%BD%BF%E7%94%A8&qid=1729343808&sprefix=%E3%83%99%E3%83%93%E3%83%BC%2B%E3%82%AC%E3%83%BC%E3%82%BC%2B%E8%9B%8D%E5%85%89%E5%A1%97%E6%96%99%E4%B8%8D%E4%BD%BF%E7%94%A8%2Caps%2C165&sr=8-13&th=1" target="_blank" rel="nofollow noopener" className="text-sm mt-2 text-blue-500 underline block">
+                                    [MOMSMENU] ガーゼ ハンカチ ベビー 4重ガーゼのタオル 綿100% 柔らかい 赤ちゃんガーゼ 30x30㎝ 10枚
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="size-4 inline-block ml-1 mt-[-3px]">
+                                        <path d="M6.22 8.72a.75.75 0 0 0 1.06 1.06l5.22-5.22v1.69a.75.75 0 0 0 1.5 0v-3.5a.75.75 0 0 0-.75-.75h-3.5a.75.75 0 0 0 0 1.5h1.69L6.22 8.72Z" />
+                                        <path d="M3.5 6.75c0-.69.56-1.25 1.25-1.25H7A.75.75 0 0 0 7 4H4.75A2.75 2.75 0 0 0 2 6.75v4.5A2.75 2.75 0 0 0 4.75 14h4.5A2.75 2.75 0 0 0 12 11.25V9a.75.75 0 0 0-1.5 0v2.25c0 .69-.56 1.25-1.25 1.25h-4.5c-.69 0-1.25-.56-1.25-1.25v-4.5Z" />
+                                    </svg>
+                                </a>
                             </div>
                             <div className="flex-1 basis-1/5 flex flex-col justify-center items-center text-gray-500">
                                 <span className="rounded-full p-1 bg-gray-100">
@@ -384,13 +391,13 @@ export default function ItemSection() {
                                     </svg>
                                 </a>
                             </div>
-                            <div className="flex-1 basis-1/5 flex flex-col justify-center items-center text-gray-500">
-                                <span className="rounded-full p-1 bg-gray-100">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4">
+                            <div className="flex-1 basis-1/5 flex flex-col justify-center items-center">
+                                <span className="rounded-full p-1 bg-green-500">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4 text-white">
                                         <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
                                     </svg>
                                 </span>
-                                <span className="text-xs mt-1">未購入</span>
+                                <span className="text-xs mt-1">買わない</span>
                             </div>
                         </li>
                         <li className="mt-4 p-4 rounded-xl bg-white flex items-center">
@@ -1006,6 +1013,13 @@ export default function ItemSection() {
                             <div className="flex-1 basis-4/5">
                                 <h4 className="font-medium">おくるみ</h4>
                                 <p className="text-sm mt-2">1〜2枚<br />素材：フリース・ポリエステル（冬）、コットン（春・秋）、ガーゼ（夏）</p>
+                                <a href="https://www.amazon.co.jp/%E3%81%8A%E3%81%8F%E3%82%8B%E3%81%BF%E3%80%81Horyku-%E3%83%99%E3%83%93%E3%83%BC%E3%83%96%E3%83%A9%E3%83%B3%E3%82%B1%E3%83%83%E3%83%88-%E3%83%99%E3%83%93%E3%83%BC%E3%83%90%E3%82%B9%E3%82%BF%E3%82%AA%E3%83%AB-%E3%82%AC%E3%83%BC%E3%82%BC%E3%82%BF%E3%82%AA%E3%83%AB%E3%81%AB%E3%81%8A%E3%81%99%E3%81%99%E3%82%81-120x120cm/dp/B07K45S4C9/ref=mp_s_a_1_6?crid=15XI4RYSGGZWX&dib=eyJ2IjoiMSJ9.k17z3l8Beu01to7BDpckGie0z8T_IBLM4HtnQoOqE8cl6LiLcQKposLoDDvlugr8kBqlT-boX5q3gG9xscqtP4a288sz8PpQXffIwdGzZyiT-S-dZw_J_vAjp2mQEP1wty9sBXE5XKGyUDxhp9tBkuje_jZQQpsTCplAqNl6m4CVVmNb8-pzJjEY7lYdZQwsT8u37q0xfmMAFFD6njw02Q.rdtdTKipFSzIeEVGRBYdJbr8-f7lbeO96pxXVVHI0gQ&dib_tag=se&keywords=%E3%82%AC%E3%83%BC%E3%82%BC%2B%E3%81%8A%E3%81%8F%E3%82%8B%E3%81%BF&qid=1729344432&sprefix=%E3%81%8C%E3%83%BC%E3%81%9C%E3%81%8A%E3%81%8F%E3%82%8B%E3%81%BF%2Caps%2C220&sr=8-6&th=1" target="_blank" rel="nofollow noopener" className="text-sm mt-2 text-blue-500 underline block">
+                                    おくるみ ベビーブランケット バスタオル 赤ちゃん毛布 授乳ケープ プレイマット布団 綿100％ ダブルガーゼ 可愛い 保温 吸水 速乾 新生児 出産祝い ガーゼタオルに 120x120cm (二枚セット)
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="size-4 inline-block ml-1 mt-[-3px]">
+                                        <path d="M6.22 8.72a.75.75 0 0 0 1.06 1.06l5.22-5.22v1.69a.75.75 0 0 0 1.5 0v-3.5a.75.75 0 0 0-.75-.75h-3.5a.75.75 0 0 0 0 1.5h1.69L6.22 8.72Z" />
+                                        <path d="M3.5 6.75c0-.69.56-1.25 1.25-1.25H7A.75.75 0 0 0 7 4H4.75A2.75 2.75 0 0 0 2 6.75v4.5A2.75 2.75 0 0 0 4.75 14h4.5A2.75 2.75 0 0 0 12 11.25V9a.75.75 0 0 0-1.5 0v2.25c0 .69-.56 1.25-1.25 1.25h-4.5c-.69 0-1.25-.56-1.25-1.25v-4.5Z" />
+                                    </svg>
+                                </a>
                             </div>
                             <div className="flex-1 basis-1/5 flex flex-col justify-center items-center text-gray-500">
                                 <span className="rounded-full p-1 bg-gray-100">
@@ -1117,22 +1131,8 @@ export default function ItemSection() {
                         <li className="mt-4 p-4 rounded-xl bg-white flex items-center">
                             <div className="flex-1 basis-4/5">
                                 <h4 className="font-medium">空気清浄機</h4>
-                                <a href="https://panasonic.jp/airrich/products/F-VXW70.html" target="_blank" rel="nofollow noopener" className="text-sm mt-2 text-blue-500 underline block">
-                                    Panasonic F-VXW70-W
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="size-4 inline-block ml-1 mt-[-3px]">
-                                        <path d="M6.22 8.72a.75.75 0 0 0 1.06 1.06l5.22-5.22v1.69a.75.75 0 0 0 1.5 0v-3.5a.75.75 0 0 0-.75-.75h-3.5a.75.75 0 0 0 0 1.5h1.69L6.22 8.72Z" />
-                                        <path d="M3.5 6.75c0-.69.56-1.25 1.25-1.25H7A.75.75 0 0 0 7 4H4.75A2.75 2.75 0 0 0 2 6.75v4.5A2.75 2.75 0 0 0 4.75 14h4.5A2.75 2.75 0 0 0 12 11.25V9a.75.75 0 0 0-1.5 0v2.25c0 .69-.56 1.25-1.25 1.25h-4.5c-.69 0-1.25-.56-1.25-1.25v-4.5Z" />
-                                    </svg>
-                                </a>
-                                <a href="https://www.amazon.co.jp/s?k=Panasonic+F-VXW70-W&__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&crid=3KCBZ1YZPQJ18&sprefix=panasonic+f-vxw70-w%2Caps%2C175&ref=nb_sb_noss" target="_blank" rel="nofollow noopener" className="text-sm mt-2 text-blue-500 underline block">
-                                    Panasonic F-VXW70-W 一覧 Amazon
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="size-4 inline-block ml-1 mt-[-3px]">
-                                        <path d="M6.22 8.72a.75.75 0 0 0 1.06 1.06l5.22-5.22v1.69a.75.75 0 0 0 1.5 0v-3.5a.75.75 0 0 0-.75-.75h-3.5a.75.75 0 0 0 0 1.5h1.69L6.22 8.72Z" />
-                                        <path d="M3.5 6.75c0-.69.56-1.25 1.25-1.25H7A.75.75 0 0 0 7 4H4.75A2.75 2.75 0 0 0 2 6.75v4.5A2.75 2.75 0 0 0 4.75 14h4.5A2.75 2.75 0 0 0 12 11.25V9a.75.75 0 0 0-1.5 0v2.25c0 .69-.56 1.25-1.25 1.25h-4.5c-.69 0-1.25-.56-1.25-1.25v-4.5Z" />
-                                    </svg>
-                                </a>
-                                <a href="https://www.ksdenki.com/shop/g/g4573535312024/" target="_blank" rel="nofollow noopener" className="text-sm mt-2 text-blue-500 underline block">
-                                    ダイキン MCK554AKS-W
+                                <a href="https://www.amazon.co.jp/%E3%83%91%E3%83%8A%E3%82%BD%E3%83%8B%E3%83%83%E3%82%AF-%E7%A9%BA%E6%B0%97%E6%B8%85%E6%B5%84%E6%A9%9F-%E3%82%A8%E3%82%B3%E3%83%8A%E3%83%93%E6%90%AD%E8%BC%89-3D%E3%83%95%E3%83%AD%E3%83%BC%E8%8A%B1%E7%B2%89%E6%92%83%E9%80%80%E6%B0%97%E6%B5%81%E3%81%A7%E8%8A%B1%E7%B2%89%E3%82%92%E3%83%91%E3%83%AF%E3%83%95%E3%83%AB%E3%81%AB%E5%90%B8%E5%BC%95-F-VC70XV-W/dp/B0BGPQWQTT/ref=sr_1_4?__mk_ja_JP=%E3%82%AB%E3%82%BF%E3%82%AB%E3%83%8A&crid=3KCBZ1YZPQJ18&dib=eyJ2IjoiMSJ9.W3OuuaEkj6cgh9HElD_QmsSgVSnxtYstKwgjg5nijEVVrfIXkL5HC9tRuchlt_Y03unZ1KEEl5N6UGiiVqfTGlU-EnfE7uRIVxlVsTuFgMk7QWm-cy_0TC7C4tccqrO_gJsWDdAfnGUK-d5sX6RbXT8zXuOyH1KJ7VkcmQa4aIJ0MrSyPB9Kb8Etn979JuiVf0hmeZJ9VEvSkUPldzT3cbsFnShncMO9ld33ZM64eYVi6mIo5aFVRPvlgdCBhJ2TXwxfRp6zBtrtbAup3rIsOVzE5dMxcj1s3tNjhVTzpxQ.KWFQ1LTlKW3rviYqwz8Mvrswsi0KBr9lbBF8BTE7G8I&dib_tag=se&keywords=Panasonic%2BF-VXW70-W&qid=1729382733&sprefix=panasonic%2Bf-vxw70-w%2Caps%2C175&sr=8-4&th=1" target="_blank" rel="nofollow noopener" className="text-sm mt-2 text-blue-500 underline block">
+                                    Panasonic F-VC70XV-W
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="size-4 inline-block ml-1 mt-[-3px]">
                                         <path d="M6.22 8.72a.75.75 0 0 0 1.06 1.06l5.22-5.22v1.69a.75.75 0 0 0 1.5 0v-3.5a.75.75 0 0 0-.75-.75h-3.5a.75.75 0 0 0 0 1.5h1.69L6.22 8.72Z" />
                                         <path d="M3.5 6.75c0-.69.56-1.25 1.25-1.25H7A.75.75 0 0 0 7 4H4.75A2.75 2.75 0 0 0 2 6.75v4.5A2.75 2.75 0 0 0 4.75 14h4.5A2.75 2.75 0 0 0 12 11.25V9a.75.75 0 0 0-1.5 0v2.25c0 .69-.56 1.25-1.25 1.25h-4.5c-.69 0-1.25-.56-1.25-1.25v-4.5Z" />
@@ -1151,7 +1151,7 @@ export default function ItemSection() {
                         <li className="mt-4 p-4 rounded-xl bg-white flex items-center">
                             <div className="flex-1 basis-4/5">
                                 <h4 className="font-medium">クローゼットケース</h4>
-                                <p className="text-sm mt-2">おむつのストック</p>
+                                <p className="text-sm mt-2">おむつのストック、ニトリ</p>
                             </div>
                             <div className="flex-1 basis-1/5 flex flex-col justify-center items-center">
                                 <span className="rounded-full p-1 bg-green-500">
